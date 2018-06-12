f39bc95c240718b44e30c6193b0205942476709d
Yi.Lu <yi.lu@deutschepost.de>
2018-06-12 13:15:34 +0200

    added mission code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AstronautComponent } from './astronaut/astronaut.component';
+import { MissionControlComponent } from './mission-control/mission-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AstronautComponent,
+    MissionControlComponent
   ],
   imports: [
     BrowserModule
